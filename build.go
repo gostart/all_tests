0896ed60ef1b2bb54f665e668814d99a69ec3a50
Erik Unger <erik@erikunger.com>
2014-07-08 17:21:14 +0200

    added build.go

---
+++ build.go
@@ -0,0 +1,13 @@
+package all_tests
+
+// Dummy import all packages to build them
+import (
+	_ "github.com/gostart/debug"
+	_ "github.com/gostart/errs"
+	_ "github.com/gostart/model"
+	_ "github.com/gostart/view"
+	_ "github.com/gostart/view/html"
+	_ "github.com/gostart/view/html/bootstrap"
+	_ "github.com/gostart/view/json"
+	_ "github.com/gostart/view/xml"
+)
